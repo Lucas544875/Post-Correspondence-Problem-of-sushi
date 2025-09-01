b050cdd44ac5e6c06b4e29a3870c52ed0ceeeb06
Lucas544875 <takayanagi691@gmail.com>
2025-09-01 21:47:42 +0900

    [update] チュートリアル問題の整備

--- src/data/problems.ts
+++ src/data/problems.ts
@@ -31,6 +31,48 @@ export const sampleProblems: Problem[] = [
     ]
   },
   {
+    id: '3',
+    difficulty: 'np-hard',
+    solvable: true,
+    solution: [0,0,1],
+    initialState: {
+      topBelt: 'TST',
+      bottomBelt: ""
+    },
+    tiles: [
+      { id: 'tile-1', top: 'S', bottom: 'ST' },
+      { id: 'tile-2', top: 'T', bottom: 'TS' },
+    ]
+  },
+  {
+    id: '4',
+    difficulty: 'np-hard',
+    solvable: true,
+    solution: [1,1,0,1],
+    initialState: {
+      topBelt: 'STSS',        // STS
+      bottomBelt: ''    // TST
+    },
+    tiles: [
+      { id: 'tile-1', top: 'SS', bottom: 'STT' },
+      { id: 'tile-2', top: 'TS', bottom: 'TST' },
+    ]
+  },
+  {
+    id: '5',
+    difficulty: 'np-hard',
+    solvable: true,
+    solution: [0,1,0,0,1,0],
+    initialState: {
+      topBelt: 'TSST',
+      bottomBelt: ''
+    },
+    tiles: [
+      { id: 'tile-1', top: 'S', bottom: 'ST' },
+      { id: 'tile-2', top: 'TST', bottom: 'TST' },
+    ]
+  },
+  {
     id: '103',
     difficulty: 'undecidable',
     solvable: false,
